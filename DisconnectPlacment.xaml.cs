43f020838e3e38fa818c20263fa723c3db95f7c5
travispotterBH <travis.potter@burohappold.com>
2023-03-17 12:29:52 -0400

    Add project files.

---
+++ DisconnectPlacment.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DisconnectPlacement
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class DisconnectPlacement: Window
+    {
+        public DisconnectPlacement()
+        {
+            InitializeComponent();
+        }
+
+        private void PlaceDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoadElements_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
